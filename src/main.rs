40fa739360d39afe5a1b8b4d56f4c8dc59b21ade
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2023-05-14 20:58:39 +0100

    Added CLI parsing and basic file reading functionality. (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use clap::Parser;
+use std::fs;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    file: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("In file {:?}", args.file);
+    println!("In file {:?}", args.file.canonicalize());
+
+    let Ok(file_path) = args.file.canonicalize() else {
+        panic!("Invalid file '{:?}'", args.file)
+    };
+
+    let contents = fs::read_to_string(file_path).expect("Unable to read file.");
+
+    for line in contents.lines().map(|line| line.trim()) {
+        println!("Line:{line}")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        assert_eq!(1, 1);
+    }
 }
